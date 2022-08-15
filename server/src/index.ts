d24af9dc87fe39b2730da6c55308cc9126077767
iwanhae <iwanhae@gmail.com>
2022-08-15 23:48:33 +0900

    use XO

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,60 +1,63 @@
-console.log({ message: "starting..." })
+import {constants} from 'node:http2';
+import process from 'node:process';
 import Koa from 'koa';
 import Router from 'koa-router';
-import Static from 'koa-static';
-import Send from 'koa-send';
-import Proxy from 'koa-proxy';
+import koaStatic from 'koa-static';
+import koaSend from 'koa-send';
+import koaProxy from 'koa-proxy';
 import server from '@/server';
-import { constants } from 'http2'
+
+console.log({message: 'starting...'});
 
 const app: Koa = new Koa();
-const port: number | string = process.env.PORT || 3000;
-const proxy: string | undefined = process.env.PROXY || undefined;
+const port: number | string = process.env.PORT ?? 3000;
+const proxy: string | undefined = process.env.PROXY ?? undefined;
 
-app.use(async (ctx, next) => {
-    await next();
-    const rt = ctx.response.get('X-Response-Time');
-    console.log({
-        method: ctx.method,
-        response: ctx.status,
-        host: ctx.host,
-        path: ctx.path,
-        request_url: ctx.URL.href,
-        duration: rt,
-    });
-})
+(async () => {
+	app.use(async (ctx, next) => {
+		await next();
+		const rt = ctx.response.get('X-Response-Time');
+		console.log({
+			method: ctx.method,
+			response: ctx.status,
+			host: ctx.host,
+			path: ctx.path,
+			request_url: ctx.URL.href,
+			duration: rt,
+		});
+	});
 
-app.use(async (ctx, next) => {
-    const start = Date.now();
-    await next();
-    const ms = Date.now() - start;
-    ctx.set('X-Response-Time', `${ms}ms`);
-});
+	app.use(async (ctx, next) => {
+		const start = Date.now();
+		await next();
+		const ms = Date.now() - start;
+		ctx.set('X-Response-Time', `${ms}ms`);
+	});
 
-const router: Router = new Router({
-    prefix: "/auth",
-});
-// handle apis
-server(router)
-app.use(router.routes());
-app.use(router.allowedMethods());
+	const router: Router = new Router({
+		prefix: '/auth',
+	});
+	// Handle apis
+	await server(router);
+	app.use(router.routes());
+	app.use(router.allowedMethods());
 
-if (proxy === undefined) {
-    // handle statics
-    app.use(Static("dist", { maxAge: 1000 }))
-    // default index.html
-    app.use(async (ctx, next) => {
-        ctx.status = constants.HTTP_STATUS_NOT_FOUND
-        await Send(ctx, "dist/index.html")
-    })
-} else {
-    console.log({ message: "use proxy mode", proxy })
-    app.use(Proxy({
-        host: proxy
-    }));
-}
+	if (proxy === undefined) {
+		// Handle statics
+		app.use(koaStatic('dist', {maxAge: 1000}));
+		// Default index.html
+		app.use(async (ctx, next) => {
+			ctx.status = constants.HTTP_STATUS_NOT_FOUND;
+			await koaSend(ctx, 'dist/index.html');
+		});
+	} else {
+		console.log({message: 'use proxy mode', proxy});
+		app.use(koaProxy({
+			host: proxy,
+		}));
+	}
 
-
-// listen
-console.log({ listen: port })
-app.listen(port);
+	// Listen
+	console.log({listen: port});
+	app.listen(port);
+})();
--- server/src/server/index.ts
+++ server/src/server/index.ts
@@ -1,45 +1,46 @@
-import axios, { AxiosError } from 'axios';
-import { constants } from 'http2';
-import jose from 'jose'
+import {constants} from 'node:http2';
+import axios, {AxiosError} from 'axios';
+import jose from 'jose';
 import Router from 'koa-router';
-import qs from 'qs'
-import { OIDCClient } from '@/oidc'
+import qs from 'qs';
+import {OidcClient} from '@/oidc';
 
-const kakao_oidc = new OIDCClient({
-    configuration_uri: "https://kauth.kakao.com/.well-known/openid-configuration",
-    client_id: "a8b1ecbe88809e47c378b1fdce1e1d61",
-    redirect_uri: "http://127.0.0.1:3000/auth/oauth/kakao"
-})
+export const COOKIE_ID_TOKEN = 'id_token';
 
-const COOKIE_ID_TOKEN = "id_token"
+const kakao_oidc = new OidcClient({
+	configuration_uri: 'https://kauth.kakao.com/.well-known/openid-configuration',
+	client_id: 'a8b1ecbe88809e47c378b1fdce1e1d61',
+	redirect_uri: 'http://127.0.0.1:3000/auth/oauth/kakao',
+});
 
-export default async (router: Router) => {
-    await kakao_oidc.init()
+const buildRouter = async (router: Router) => {
+	await kakao_oidc.init();
 
+	router.get('/', (ctx, next) => {
+		ctx.body = {hello: 'world'};
+	});
 
-    router.get('/', (ctx, next) => {
-        ctx.body = { hello: "world" };
-    });
+	router.get('/login/kakao', (ctx, next) => {
+		ctx.redirect(kakao_oidc.getAuthorizationRedirectURI().href);
+	});
 
-    router.get('/login/kakao', (ctx, next) => {
-        ctx.redirect(kakao_oidc.getAuthorizationRedirectURI().href)
-    })
+	router.get('/oauth/kakao', async (ctx, next) => {
+		const code = ctx.URL.searchParams.get('code') ?? '';
+		if (code === '') {
+			ctx.status = constants.HTTP_STATUS_BAD_REQUEST;
+			ctx.body = {message: 'invalid request'};
+			return;
+		}
 
-    router.get("/oauth/kakao", async (ctx, next) => {
-        const code = ctx.URL.searchParams.get('code') || ""
-        if (code === "") {
-            ctx.status = constants.HTTP_STATUS_BAD_REQUEST
-            ctx.body = { message: "invalid request" }
-            return
-        }
+		const token = await kakao_oidc.fetchTokenUsingCode(code);
+		console.log(await kakao_oidc.fetchUserinfo(token.access_token));
+		ctx.cookies.set(COOKIE_ID_TOKEN, token.id_token, {
+			overwrite: true,
+			httpOnly: false,
+		});
 
-        const token = await kakao_oidc.fetchTokenUsingCode(code)
-        console.log(await kakao_oidc.fetchUserinfo(token.access_token))
-        ctx.cookies.set(COOKIE_ID_TOKEN, token.id_token, {
-            overwrite: true,
-            httpOnly: false
-        })
+		ctx.redirect('/');
+	});
+};
 
-        ctx.redirect("/")
-    })
-}
+export default buildRouter;
